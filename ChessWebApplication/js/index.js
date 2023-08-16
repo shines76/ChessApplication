a6e266f5180df7ad2b022ca78872375ca978e417
Sanderson Hines <sandersonhines76@yahoo.com>
2023-08-16 09:35:26 -0500

    getting ready to move to .net

--- ChessWebApplication/js/index.js
+++ ChessWebApplication/js/index.js
@@ -1,38 +1,70 @@
+
 const apiUrl = "http://127.0.0.1:5000"
-
 const jsonData = { "team": "white" }
+const x_axis = ["a", "b", "c", "d", "e", "f", "g", "h"];
+const y_axis = [1, 2, 3, 4, 5, 6, 7, 8]
 
-var pythonData;
+async function onClickPlay() {
 
-var output = document.getElementById("output");
+  try {
+    let data = await getChessData();
+    console.log(data.boardString);
+    return data;
+  } catch (error) {
 
-function start_game() {
+    console.error('Error', error);
 
-  fetch(apiUrl + "/start", {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(jsonData)
-  })
-    .then(response => response.json())
-    .then(data => {
-      // Handle the response from the server
-      pythonData = data;
-      console.log(pythonData);
-      output.innerHTML = pythonData;
-    })
-    .catch(error => {
-      // Handle errors
-      console.error('Error:', error);
-    });
+  }
 
 }
 
+const pythonData = onClickPlay();
 
-function move_piece() {
+async function buildBoard() {
+
+}
+async function buildTiles() {
+
+
+  //return [{{xy}, {"piece"}}, ...]
+
+}
+
+async function setBoard(boardString) {
+  return {
+    boardString,
+    tiles: buildTiles(),
+    buildBoard: function () {
+
+
+    }
+  }
+}
+
+const board = setBoard(pythonData.boardString,)
+
+
+
+async function getChessData() {
+  try {
+    const response = await fetch(apiUrl + "/start", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(jsonData)
+    });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function move_piece() {
   console.log(pythonData.boardString);
 
   output.innerHTML += "<br><br><br>" + pythonData.boardString;
 
 }
+
